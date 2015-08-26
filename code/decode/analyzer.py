2798d0630407b40f236b109851e00eeb35a7701a
atong01 <alexanderytong@gmail.com>
2015-08-26 15:21:30 -0700

    remove boatd from repo

--- code/decode/analyzer.py
+++ code/decode/analyzer.py
@@ -10,13 +10,63 @@
 #import serial
 from time import strftime
 from datetime import datetime, time
+from decoders import *
 
 #ser = serial.Serial('/dev/ttyACM0', 115200)
 
 startTime = datetime.now()
 
-class Analyzer():
+def get_bytes(data_str, width):
+    """ Gets the bytes of a 64 bit integer in big endian format
+    """
+    data_int = int(data_str)
+    return [(data_int >> (8*i)) & 0xff for i in range(width-1, -1, -1)]
+
+class Pgn(object):
+    """ A data class for a single NMEA 2000 statement
+
+    Holds data pertaining to a single NMEA 2000 datapoint. A new Pgn object is 
+    initialized for every dataline in the log.
+
+    Attributes:
+        pgn (string) 
+        body (int)
+        data (dict)
+    """
+    VALID_PGNS = { "1F119" : attitude, 
+                   "1F802" : cog_sog_rapid_update,
+                   "1F805" : gnss_position, 
+                   "1F801" : position_rapid_update,
+                   "1F010" : sys_time, 
+                   "1F112" : vessel_heading, 
+                   "1FD02" : wind }
+
+    def __init__(self, pgn, body):
+        self.pgn  = pgn
+        self.body = body
+        self.data = self.handle_body(pgn, body)
+
+    def handle_body(self, pgn, body):
+        if self.validate(pgn, body):
+            return VALID_PGNS[pgn](body)
+        else:
+            return None
+
+    def validate(self, pgn, body):
+#TODO
+        return True
+
+    def __getitem__(self, i):
+        return self.data[i]
+
+class Analyzer(object):
+    """ A database for sensor information.
+
+    This class holds sensor information pertaining to a robotic boat, decoding
+    NMEA 2000 sentences into python readable dictionaries.
+    """
     def __init__(self):
+        self.recents  = []
         self.attitude = {"SID" : 0, "yaw" : 0}
         self.cog_sog_rapid_update = {}
         self.GNSS_position_data = {}
@@ -24,103 +74,3 @@ class Analyzer():
         self.system_time = {}
         self.vessel_heading = {}
         self.wind_data = {}
-        # id numbers for specific set of data mapped to function calls
-        self.ids = { "1F119" : SET_attitude, 
-                     "1F802" : SET_cog_sog_rapid_update,
-                     "1F805" : SET_GNSS_position_data, 
-                     "1F801" : SET_position_rapid_update,
-                     "1F010" : SET_system_time, 
-                     "1F112" : SET_vessel_heading, 
-                     "1FD02" : SET_wind_data }
-    '''
-     def SET_attitude():
-         SID = 
-         yaw = 
-         pitch = 
-         roll = 
-
-    def SET_cog_sog_rapid_update():
-         SID = 
-         cog_reference = 
-         cog = 
-         sog = 
-
-     def SET_GNSS_position_data():
-         SID = 
-         position_date = 
-         position_time = 
-         latitude = 
-         longitude = 
-         type_of_system = 
-         GNSS_method = 
-         integrity = 
-         num_SVs = 
-         HDOP = 
-         PDOP = 
-         geoidal_separation = 
-         num_reference_stations = 
-         reference_station_type = 
-         reference_station_id = 
-         age_of_DGNSS_connection = 
-
-     def SET_position_rapid_update():
-         latitude = 
-         longitude = 
-
-     def SET_system_time():
-         SID = 
-         source = 
-         date = 
-         time = 
-
-     def SET_vessel_heading():
-         SID = 
-         heading_sensor_reading = 
-         deviation = 
-         variation = 
-         heading_sensor_reference = 
-
-     def SET_wind_data():
-         SID = 
-         wind_speed = 
-         wind_direction = 
-         wind_reference = 
-    '''
-
-## analyze():
-# decodes data into acceptable format
-'''
-def run_serial():
-    analyzer = Analyzer()
-    while 1:
-        # line is a string read from the serial port, rstrip() removes
-        # trailing white spaces on the line.
-        # line=ser.readline()
-        line = ser.readline().rstrip()
-
-
-        # parses line into the data_id and the data itself
-    	data_id, data = line.partition(' ')[::2]
-        data = int(data)
-
-        analyzer.ids[data_id](data)
-'''
-
-"""
-gets the bytes of a 64 bit integer in big endian format
-"""
-def get_bytes(data_str, width):
-    data_int = int(data_str)
-    return [(data_int >> (8*i)) & 0xff for i in range(width-1, -1, -1)]
-
-def run():
-    analyzer = Analyzer()
-    while 1:
-        # parses line into the data_id and the data itself
-    	pgn, data_str = line.partition(' ')[:2]
-        data = get_bytes(data_str, 8)
-        analyzer.ids[data_id](data)
-
-
-if __name__ == "__main__":
-    run()
--- code/decode/test_analyzer.py
+++ code/decode/test_analyzer.py
@@ -1,14 +1,10 @@
-import unittest
 import analyzer
 
-class Test_Analyzer_Functions(unittest.TestCase):
+class TestAnalyzer(object):
     """ Test class for the airmar analyzer/decoder
 
-    This class should unittest decodings of specific PNG's enabled for the trst
-    project
     """
-
-    def setUp(self):
+    def setup(self):
         self.dataStrings = [2**32-1, 0x00FF7FFF7FFF7FFF]
         self.dataAnswers = [[0,0,0,0,255,255,255,255], [00, 0xFF, 0x7F, 0xFF, 0x7F, 0xFF, 0x7F, 0xFF]]
 
@@ -16,8 +12,4 @@ class Test_Analyzer_Functions(unittest.TestCase):
         """ Tests the get_bytes function in isolation """
         for data, ans in zip(self.dataStrings, self.dataAnswers):
             test = analyzer.get_bytes(data, 8)
-            self.assertEquals(test, ans)
-
-if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromTestCase(Test_Analyzer_Functions)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+            assert test == ans
