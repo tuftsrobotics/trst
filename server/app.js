87551c80f982b36728a128699dde1e60d07a13de
MorganGellert <morgangellert@gmail.com>
2015-11-02 13:10:39 -0500

    pass options to wind, pos, and waypoint

--- server/app.js
+++ server/app.js
@@ -10,12 +10,12 @@ function Application (options) {
     if (!(this instanceof Application)) {
         return new Aplication(options);
     }
-
+    var options = {};
     this.boat_data = options.boat_data || {};
     // TODO: check for boat data in all modules
-    this.Wind = options.Wind || Wind;
-    this.Position = options.Position || Position;
-    this.Waypoint = options.Waypoint || Waypoint;
+    this.Wind = options.Wind || Wind(options);
+    this.Position = options.Position || Position(options);
+    this.Waypoint = options.Waypoint || Waypoint(options);
 
     this.app = options.app || express();
     this.app.use(bodyParser.urlencoded({extended: false}));
