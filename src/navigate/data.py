9abb5b16c38303a9706fabed4f752699e94101ec
atong01 <alexanderytong@gmail.com>
2015-11-02 13:06:38 -0500

    navigator software part added

--- src/navigate/data.py
+++ src/navigate/data.py
@@ -2,12 +2,15 @@ import requests
 
 server_url = '127.0.0.1'
 server_port = '8888'
-server      = server_url + ':' + server_port + '/'
+#server_port = '2222' # boatd
+server      = 'http://' + server_url + ':' + server_port + '/'
 
 def request(string):
     """ gets the data specified by the identifier """
 
     r = requests.get(server + string)
+    if r.status_code == 404:
+        print "ERROR 404 string not found"
     if r.status_code != 200:
         print "BAD STRING DYING"
         return -1
--- src/navigate/navigator.py
+++ src/navigate/navigator.py
@@ -5,3 +5,23 @@
 #
 
 
+from LatLon import LatLon
+import data
+
+def get_vect_to_wp(p):
+    c = data.request('gps')
+    print c.json()
+
+def get_vect(p1, p2):
+    """returns heading and magnitude between two latlon objects """
+    assert type(p1) == type(LatLon)
+    assert type(p2) == type(LatLon)
+    return p2 - p1
+
+
+def main():
+    get_vect_to_wp(LatLon(1,1))
+    pass
+
+if __name__ == '__main__':
+    main()
