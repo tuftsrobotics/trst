d499f7279a8b0d3527447e193a1a5e9e082761c1
atong01 <alexanderytong@gmail.com>
2015-11-19 10:35:53 -0500

    new flask server replacing node server for simplicity

---
+++ src/pi2/server.py
@@ -0,0 +1,30 @@
+"""
+Implements a server on port 8888 with simple json posting
+API:
+    '/' all data unioned
+    '/waypoints' waypoint data
+"""
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+class server(object):
+    data = {}
+    waypoints = {}
+
+    @app.route('/', methods = ['GET', 'POST'])
+    def index_post():
+        if request.method == 'POST':
+            server.data.update(request.json)
+        return jsonify(server.data)
+
+    @app.route('/waypoint', methods = ['GET', 'POST'])
+    def waypoint_method():
+        if request.method == 'POST':
+            server.waypoints.update(request.json)
+        return jsonify(server.waypoints)
+
+if __name__ == "__main__":
+    s = server()
+    app.run(debug=True, port = 8888)
+
