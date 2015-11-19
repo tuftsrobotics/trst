2cf92d26f5d9afc21f8627db8369382fb46138f3
atong01 <alexanderytong@gmail.com>
2015-11-19 14:57:27 -0500

    continued development of gpx file compatability

--- src/pi2/set_waypoint.py
+++ src/pi2/set_waypoint.py
@@ -6,8 +6,19 @@ where each line represents a waypoint
 from argparse import ArgumentParser
 import csv
 import data
+import gpxpy
 
-def main(infile, logfilenum):
+def gpx_main(infile, logfilenum):
+    gpx_file = open(infile)
+    d = {}
+    gpx = gpxpy.parse(gpx_file)
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for i, point in enumerate(segment.points):
+                d[str(i)] = {'latitude' : point.latitude, 'longitude' : point.longitude}
+    print d
+
+def csv_main(infile, logfilenum):
     data = csv.reader(open(infile))
     d = {}
     for i, (lat, lon) in enumerate(data):
@@ -15,14 +26,15 @@ def main(infile, logfilenum):
         d[key] = {'latitude' : lat, 'longitude' : lon}
     print d
 
-
-
-
-     
-
 if __name__ == '__main__':
     a = ArgumentParser()
     a.add_argument('infile', help='waypoint csv file')
     a.add_argument('-t', action = 'store', dest = 'run_number', help='run number used in logging')
+#TODO auto filetype detection
+    a.add_argument('-g', '--gpx', action = 'store_true', help='flag for gpx filetype')
     r = a.parse_args()
-    main(r.infile, r.run_number)
+    print r
+    if r.gpx:
+        gpx_main(r.infile, r.run_number)
+    else:
+        csv_main(r.infile, r.run_number)
