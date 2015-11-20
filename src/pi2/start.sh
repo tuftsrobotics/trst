f943ae0511dd97faa65d001c5a37a04f310a5cf2
atong01 <alexanderytong@gmail.com>
2015-11-20 10:11:30 -0500

    Start.sh updates, rename file, and remove server start

--- src/pi2/start.sh
+++ src/pi2/start.sh
@@ -1,27 +1,36 @@
 #/bin/bash
 
+#Start.sh
+#Start script for the trst project.
+#   0. (optional) start server
+#   1. set waypoints with a waypoint csv/gpx file
+#   2. start sensor upload on first arduino
+#   3. start navigator on second arduino
+
 i=0 #i = run number
 while [[ -e log/run/$i.log ]] ; do
     let i++
 done
 f=log/run/$i.log
-touch f
+touch $f
 echo "Starting run $i" > $f
 
-
-#up_port=/dev/tty.usbModem1411
+#sensor_port=/dev/tty.usbModem1411
 #nav_port=/dev/tty.usbModem1421
-up_port=/dev/ttyACM0
+sensor_port=/dev/ttyACM0
 nav_port=/dev/ttyACM1
+waypoints=waypoints/test
 
 ### START SERVER ###
-python server.py
+#removed for ease of control
+#python server.py &
 
 ### STAT WAYPOINT SETTER ###
+python set_waypoint.py $waypoints -g
 
 
 ### START UPLOADER ###
-python uploader.py -t $i -p $up_port
+python uploader.py -t $i -p $sensor_port
 
 
 ### START NAVIGATOR ###
