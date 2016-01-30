fa383a0d2751b9a837d1ea52a8dc163728728a3f
atong01 <alexanderytong@gmail.com>
2016-01-30 14:55:29 -0500

    added fake airmar repeating data

---
+++ src/test/fake_airmar/fake_airmar.ino
@@ -0,0 +1,18 @@
+
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+Serial.println("1F119 00FF7FFF7FFF7FFF");
+Serial.println("1F113 00FFFFFF7FFFFFFF");
+Serial.println("1F802 FFFFFFFFFFFFFFFF");
+Serial.println("1FD07 00C14380FF7FF803");
+Serial.println("1FD0A FF000024810F00FF");
+Serial.println("1FD02 00FFFFFFFFF8FFFF");
+Serial.println("1F809 FFFFFFFFFFFFFF7F");
+Serial.println("1F11A 00F5A93EFF7FFFFF");
+Serial.println("1F801 FFFFFF7FFFFFFF7F");
+Serial.println("1F805 01FFFFFFFFFFFFFF");
+}
